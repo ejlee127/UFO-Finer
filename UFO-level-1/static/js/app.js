a2f91ae34651a45ffb1f69c5cfd45c7ff7792b7e
Lyra <63160185+ejlee127@users.noreply.github.com>
2020-09-12 18:47:20 -0400

    updating pages

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,40 +1,19 @@
 // from data.js
 var tableData = data;
 
-// The dates on data;
-var dataDates = tableData.map( (ufoReport) => ufoReport.datetime);
+// The dates on the provided data;
+var datesOn = tableData.map( (ufoReport) => ufoReport.datetime)
+        .filter((x, i, a) => a.indexOf(x) == i);
 
-var  availableDates= dataDates.filter((x, i, a) => a.indexOf(x) == i)
-//d3.select("#available-date").text(availableDates);
-console.log(availableDates);
-// YOUR CODE HERE!
-
-// Select the button
+// Select the action tags and create event handlers
 var button = d3.select("#filter-btn");
-
-// Select the form
 var form = d3.select("form");
-
-// Create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
 form.on("submit",runEnter);
-var tbody = d3.select("tbody");
 
-function showDates() {
-    return " "
-    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                  <h4 class="card-title">Primary card title</h4>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-              </div>
-}
-
-
-// Create the function to run for both events
+// The function to run for both events
 function runEnter() {
-  console.log(d3.event.target);
+
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
@@ -43,19 +22,21 @@ function runEnter() {
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
+  // and then clear the input element
+  inputElement.property("value", "");
 
-  // Print the value to the console
-  console.log(inputValue);
+  // Select the tbody tag to enter data as a table
+  var tbody = d3.select("tbody");
 
-  // Set the span tag in the h1 element to the text
-  // that was entered in the form
-
-
+  // Clean up the previous results
   tbody.html("");
+  d3.select("#available-dates").html("");
 
-  if (availableDates.includes(inputValue)) {
+  // Show the table when the inputValue exits
+  if (datesOn.includes(inputValue)) {
+
+    // Filter the dates according to the input
     filteredData = tableData.filter((ufoReport) => ufoReport.datetime === inputValue );
-    console.log(filteredData);
 
     filteredData.forEach((ufoReport) => {
         var row = tbody.append("tr");
@@ -65,15 +46,30 @@ function runEnter() {
         });
     });
   }
+  //Show the avilable dates when the iput does not exist in dataset
   else {
-
+    showDates();
   }
   
 };
 
 
+function showDates() {
 
+  var divAvailable = d3.select("#available-dates")
+  var cardBody = divAvailable.append("div")
+              .classed("card", true)
+              .append("div")
+              .classed("card-body", true)
 
+  cardBody.append("h4")
+              .classed("card-title", true)
+              .text("Enter one of the following available dates:");
+
+  cardBody.append("p")
+              .classed("card-text", true)
+              .text(datesOn.map( d => " "+d))
+}
 
 
 
